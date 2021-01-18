e43b8a6900fbe026762a63ad027fa0f88f39f22c
Duncan McClean <damcclean@users.noreply.github.com>
2021-01-18 21:48:52 +0000

    Boot Samcart migrations

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,8 +6,14 @@ use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
 {
-
     protected $routes = [
         'actions' => __DIR__.'/../routes/actions.php',
     ];
+    
+    public function boot()
+    {
+        parent::boot();
+
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
 }
